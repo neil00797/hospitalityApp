38f9e9b11e0b8a8d856bdfe6e98be76bab3e688c
Christopher-96 <Smkupa@gmail.com>
2020-09-06 02:45:52 +0200

    Create OccupantMedicalDetail Repository

---
+++ src/main/java/org/hospitality/app/repository/utility/AilmentRepository.java
@@ -0,0 +1,20 @@
+package org.hospitality.app.repository.service;
+
+import org.hospitality.app.entity.service.Ailment;
+import org.hospitality.app.repository.IRepository;
+/*
+    author: @SenzoMkupa
+    date: 31-08-2020
+ */
+
+
+import java.util.Set;
+
+public interface AilmentRepository extends IRepository <Ailment, String> {
+    Set<Ailment> getAll();
+
+<<<<<<< HEAD
+}
+=======
+}
+>>>>>>> 4ab93862dbd3e0e558d97df4be06332fdbfdac0d
