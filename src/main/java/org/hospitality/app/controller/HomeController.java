e2361b12e7c2177353a3f3ea54b9a293d07b53b6
ShaunMir <49433625+ShaunMir@users.noreply.github.com>
2020-09-18 18:07:24 +0200

    Added Functionality to HomeController

--- src/main/java/org/hospitality/app/controller/HomeController.java
+++ src/main/java/org/hospitality/app/controller/HomeController.java
@@ -1,8 +1,13 @@
 package org.hospitality.app.controller;
 import org.springframework.boot.*;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 
-
+@RestController
 public class HomeController {
+    @RequestMapping({"/","/indexd.xys"})
+    String home(){
+        return "Hospitality Application running DEMO";
+    }
 }
