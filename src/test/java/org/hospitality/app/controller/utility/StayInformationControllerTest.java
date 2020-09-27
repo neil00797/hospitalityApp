1c098c32176dd3e5b427ba105eafc37b6b75d6b1
Thaakir10 <thaakir10@gmail.com>
2020-09-27 21:30:27 +0200

    #51 StayInformation Controller and test final

--- src/test/java/org/hospitality/app/controller/utility/StayInformationControllerTest.java
+++ src/test/java/org/hospitality/app/controller/utility/StayInformationControllerTest.java
@@ -2,8 +2,14 @@ package org.hospitality.app.controller.utility;
 
 import junit.framework.TestCase;
 import org.hospitality.app.entity.user.StayInformation;
+import org.hospitality.app.entity.utility.Payment;
 import org.hospitality.app.factory.user.StayInformationFactory;
+import org.hospitality.app.factory.utility.PaymentFactory;
+import org.junit.FixMethodOrder;
+import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.junit.runners.MethodSorters;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.http.HttpEntity;
@@ -16,23 +22,56 @@ import org.springframework.web.bind.annotation.RestController;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+@FixMethodOrder(MethodSorters.NAME_ASCENDING)
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 @RunWith(SpringRunner.class)
 
 public class StayInformationControllerTest
 {
+    StayInformation stayInformation = StayInformationFactory.createStayInformation( "4 days");
+
+    @Autowired
     private TestRestTemplate restTemplate;
-    private String baseURL = "http:/localhost:8080/StayInformation";
+    private String baseURL = "http:/localhost:8080/StayInformation/";
 
     public void testCreate()
     {
-        StayInformation stayInformation = StayInformationFactory.createStayInformation("4 days");
+
         String url = baseURL + "create";
         ResponseEntity<StayInformation> postResponse = restTemplate.postForEntity(url , stayInformation, StayInformation.class);
         assertNotNull(postResponse);
         assertNotNull(postResponse.getBody());
 
+        System.out.println(postResponse);
         System.out.println(postResponse.getBody());
+    }
+
+    @Test
+    public void testread() {
+        String url = baseURL + "read" + stayInformation.getStayDuration();
+        System.out.println("URL: " + url);
+        ResponseEntity<StayInformation> response = restTemplate.getForEntity(url, StayInformation.class);
+
+    }
+
+    @Test
+    public void testupdate()
+    {
+
+        StayInformation update = new StayInformation.Builder().copy(stayInformation).setStayDuration("14 Days").build();
+        String url = baseURL + "update";
+        System.out.println("URL: " + url);
+        System.out.println("Updated data: " + update);
+        ResponseEntity<StayInformation> response = restTemplate.postForEntity(url, update, StayInformation.class);
+
+    }
+
+    @Test
+    public void testdelete()
+    {
+        String url = baseURL + "delete" + stayInformation.getOccupantId();
+        System.out.println("URL: " + url);
+        restTemplate.delete(url);
     }
 
     public void testGetAll()
