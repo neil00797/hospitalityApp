f96a3582e57eb43285f16b5c684bf6c91bac2e7d
ShaunMir <49433625+ShaunMir@users.noreply.github.com>
2020-11-26 08:38:54 +0200

    Fixed some issues

--- src/main/java/org/hospitality/app/controller/utility/DeliveryController.java
+++ src/main/java/org/hospitality/app/controller/utility/DeliveryController.java
@@ -1,6 +1,7 @@
 package org.hospitality.app.controller.utility;
 
 import org.hospitality.app.entity.utility.Delivery;
+import org.hospitality.app.entity.utility.Payment;
 import org.hospitality.app.factory.utility.DeliveryFactory;
 import org.hospitality.app.service.utility.DeliveryService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -9,7 +10,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.Set;
 
 @RestController
-@RequestMapping("/delivery")
+@RequestMapping("/hospitality_db/delivery")
 public class DeliveryController {
 
     @Autowired
@@ -21,8 +22,26 @@ public class DeliveryController {
         return deliveryService.create(newDelivery);
     }
 
+    @GetMapping("/update")
+    public Delivery update(@RequestBody Delivery s)
+    {
+        return deliveryService.update(s);
+    }
+
+    @GetMapping("/read/{roomNumber}")
+    public Delivery read(@PathVariable String roomNumber)
+    {
+        return deliveryService.read(roomNumber);
+    }
+
     @GetMapping("/all")
     public Set<Delivery> getAll(){
         return deliveryService.getAll();
+    }
+
+    @DeleteMapping("/delete/{roomNumber}")
+    public boolean delete(@PathVariable String roomNumber)
+    {
+        return deliveryService.delete(roomNumber);
     }
 }
--- src/main/java/org/hospitality/app/controller/utility/PaymentController.java
+++ src/main/java/org/hospitality/app/controller/utility/PaymentController.java
@@ -1,4 +1,5 @@
 package org.hospitality.app.controller.utility;
+import org.hospitality.app.entity.user.StayInformation;
 import org.hospitality.app.entity.utility.Payment;
 import org.hospitality.app.factory.utility.PaymentFactory;
 import org.hospitality.app.service.utility.PaymentService;
@@ -8,7 +9,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.Set;
 
 @RestController
-@RequestMapping("/payment")
+@RequestMapping("/hospitality_db/payment")
 public class PaymentController
 {
 
@@ -22,10 +23,28 @@ public class PaymentController
         return paymentService.create(newPayment);
     }
 
+    @GetMapping("/read/{paymentMethod}")
+    public Payment read(@PathVariable String paymentMethod)
+    {
+        return paymentService.read(paymentMethod);
+    }
+
+    @GetMapping("/update")
+    public Payment update(@RequestBody Payment s)
+    {
+        return paymentService.update(s);
+    }
+
     @GetMapping("/all")
     public Set<Payment> getAll()
     {
         return paymentService.getAll();
 
     }
+    @DeleteMapping("/delete/{paymentMethod}")
+    public boolean delete(@PathVariable String paymentMethod)
+    {
+        return paymentService.delete(paymentMethod);
+    }
+
 }
